820aba4bd3bc2131493bacfbe759a2e62aeca1e0
Arvind Singh <c-bhattm@kapsarc.org>
2018-12-03 19:08:43 +0530

    test

--- Fs-frontend/src/app/theme/dashboard/vat-management/edit-vat/edit-vat.component.ts
+++ Fs-frontend/src/app/theme/dashboard/vat-management/edit-vat/edit-vat.component.ts
@@ -23,7 +23,7 @@ export class EditVatComponent implements OnInit {
   states: Array<Object> = [];
   vatForm: FormGroup;
   paymentMode = [
-    { 'id': 1, '  ': 'Paypal' },
+    { 'id': 1, 'itemName': 'Paypal' },
     { 'id': 2, 'itemName': 'Stripe' },
     { 'id': 3, 'itemName': 'Bank Transfer' }
   ]
@@ -41,8 +41,7 @@ export class EditVatComponent implements OnInit {
 
     this.currentVat.state.forEach(state => {
       if (state.name) {
-        let element = { 'id': state.id, 'itemName': state.name };
-        this.selectedStates.push(element);
+        this.selectedStates.push({ 'id':  (state.id).toString(), 'itemName': (state.name).toString() });
       }
     })
 
