5736aa53264f406e8c476a048ae11cb108267433
mahesh <teamm@synapseindia.com>
2018-12-28 20:34:13 +0530

    new changes in notifications

--- Fs-frontend/src/app/theme/dashboard/my-plan/change-subscription-plan/supplier-plans/supplier-plans.component.ts
+++ Fs-frontend/src/app/theme/dashboard/my-plan/change-subscription-plan/supplier-plans/supplier-plans.component.ts
@@ -1,5 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
-
+import { Component, OnInit, Input, ViewChild, ElementRef, HostListener, Renderer2 } from '@angular/core';
 @Component({
   selector: 'app-supplier-plans',
   templateUrl: './supplier-plans.component.html',
@@ -7,14 +6,44 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class SupplierPlansComponent implements OnInit {
 
-  @Input('userRole') userRole: any;
-  @Input('plans') plans: any;
-  @Input('currentUser') currentUser: any;
-  showMessage: any;
 
-  constructor() { }
+  active1 = true;
+  applyClass = 1;
+  @ViewChild('supplier1') supplier1: ElementRef;
+  @ViewChild('supplier2') supplier2: ElementRef;
+  @ViewChild('stockist1') stockist1: ElementRef;
+  @ViewChild('stockist2') stockist2: ElementRef;
+  @ViewChild('reseller1') reseller1: ElementRef;
+  @ViewChild('reseller2') reseller2: ElementRef;
+  @ViewChild('popupDiv') popupDiv: ElementRef;
 
+  @HostListener('click', ['$event'])
+  onclick(event: MouseEvent) {
+
+  }
+  constructor(private renderer: Renderer2) { }
   ngOnInit() {
+
+  }
+
+  classToApply(event) {
+    if (event.target.text === "supplier")
+      this.applyClass = 1;
+    else if (event.target.text === "stockist")
+      this.applyClass = 2;
+    else
+      this.applyClass = 3;
+  }
+
+  onMouseOver() {
+    this.renderer.setStyle(this.popupDiv.nativeElement, 'display', 'block')
+  }
+
+  onMouseLeave() {
+    this.renderer.setStyle(this.popupDiv.nativeElement, 'display', 'none')
   }
 
 }
+
+
+
