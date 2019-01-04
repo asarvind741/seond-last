03f92f4f65ab2b4f9179b461b4afb66eab24f678
Siddharth Mishra <sidmishra@synapseco.com>
2019-01-04 12:46:43 +0530

    added admin setting

--- Fs-frontend/src/app/theme/dashboard/user-management/add-user/add-user.component.ts
+++ Fs-frontend/src/app/theme/dashboard/user-management/add-user/add-user.component.ts
@@ -16,12 +16,12 @@ export class AddUserComponent implements OnInit {
   statuss: Array<String> = ['Active', 'Inactive'];
   roles: Array<String> = ['Buyer', 'Seller', 'Admin', 'SubAdmin', 'Agent', 'Reseller'];
   permission: Array<any> = [
-    { 'id': 1, 'itemName': 'isAdmin' }, 
-    { 'id': 2, 'itemName': 'isSubAdmin' }, 
-    { 'id': 3, 'itemName': 'isBuyer' }, 
-    { 'id': 4, 'itemName': 'isSupplier' }, 
-    { 'id': 5, 'itemName': 'isReseller' }, 
-    { 'id': 6, 'itemName': 'isAgent' }, 
+    { 'id': 1, 'itemName': 'isAdmin' },
+    { 'id': 2, 'itemName': 'isSubAdmin' },
+    { 'id': 3, 'itemName': 'isBuyer' },
+    { 'id': 4, 'itemName': 'isSupplier' },
+    { 'id': 5, 'itemName': 'isReseller' },
+    { 'id': 6, 'itemName': 'isAgent' },
     { 'id': 7, 'itemName': 'isAccountAdmin' }]
   selectedPermission: any = [];
   showPermissionFlag: boolean = false;
@@ -54,35 +54,31 @@ export class AddUserComponent implements OnInit {
   }
 
   addNewUser() {
-    // console.log("this form", this.newUserForm.value);
-    console.log("selectedPermission----------------->>",this.selectedPermission)
-    let permission = {isAdmin: false, isSubAdmin: false, isBuyer: false, isSupplier: false, isReseller: false, isAgent: false, isAccountAdmin: false}
-    if(this.selectedPermission.length > 0 && this.newUserForm.value.role == 'SubAdmin'){
+    let permission = { isAdmin: false, isSubAdmin: false, isBuyer: false, isSupplier: false, isReseller: false, isAgent: false, isAccountAdmin: false }
+    if (this.selectedPermission.length > 0 && this.newUserForm.value.role == 'SubAdmin') {
       this.selectedPermission.forEach(element => {
-        element.itemName == 'isAdmin' ? permission.isAdmin = true: permission.isAdmin = false;
-        element.itemName == 'isSubAdmin' ? permission.isSubAdmin = true: permission.isSubAdmin = false;
-        element.itemName == 'isSubAdmin' ? permission.isBuyer = true: permission.isBuyer = false;
-        element.itemName == 'isSubAdmin' ? permission.isSupplier = true: permission.isSupplier = false;
-        element.itemName == 'isSubAdmin' ? permission.isReseller = true: permission.isReseller = false;
-        element.itemName == 'isSubAdmin' ? permission.isAgent = true: permission.isAgent = false;
-        element.itemName == 'isSubAdmin' ? permission.isAccountAdmin = true: permission.isAccountAdmin = false;
+        if (element.itemName == 'isAdmin') permission.isAdmin = true;
+        if (element.itemName == 'isSubAdmin') permission.isSubAdmin = true;
+        if (element.itemName == 'isBuyer') permission.isBuyer = true;
+        if (element.itemName == 'isSupplier') permission.isSupplier = true;
+        if (element.itemName == 'isReseller') permission.isReseller = true;
+        if (element.itemName == 'isAgent') permission.isAgent = true;
+        if (element.itemName == 'isAccountAdmin') permission.isAccountAdmin = true;
       });
-      this.newUserForm.value.permissions = permission;
     }
-    console.log("this form", this.newUserForm.value);
-
-    // this.userService.addUser(this.newUserForm.value)
-    //   .subscribe((response: HttpResponse<any>) => {
-    //     if (response.status === 200) {
-    //       this.closeModal();
-    //       this.openSuccessSwal();
-    //     }
-    //   }, (error) => {
-    //     this.closeModal();
-    //     this.showMessage = error.error['message']
-    //     this.openUnscuccessSwal();
-    //   })
-
+    this.newUserForm.value.permissions = permission;
+    console.log("this form", JSON.stringify(this.newUserForm.value));
+    this.userService.addUser(this.newUserForm.value)
+      .subscribe((response: HttpResponse<any>) => {
+        if (response.status === 200) {
+          this.closeModal();
+          this.openSuccessSwal();
+        }
+      }, (error) => {
+        this.closeModal();
+        this.showMessage = error.error['message']
+        this.openUnscuccessSwal();
+      })
   }
 
   openSuccessSwal() {
--- Fs-frontend/src/app/theme/dashboard/user-management/edit-user/edit-user.component.ts
+++ Fs-frontend/src/app/theme/dashboard/user-management/edit-user/edit-user.component.ts
@@ -4,6 +4,9 @@ import swal from 'sweetalert2';
 import { FormArray, FormGroup, FormControl, Validators } from '@angular/forms';
 import { UserService } from '../../../../services/user.servivce';
 import { HttpResponse } from '@angular/common/http';
+import { element } from '@angular/core/src/render3';
+import { Item } from 'angular2-multiselect-dropdown';
+import { stringify } from '@angular/compiler/src/util';
 
 @Component({
   selector: 'app-edit-user',
@@ -15,25 +18,59 @@ export class EditUserComponent implements OnInit {
   showMessage: any;
   statuss: Array<String> = ['Active', 'Inactive'];
   roles: Array<String> = ['Buyer', 'Seller', 'Admin', 'SubAdmin'];
+  permission: Array<any> = [
+    { 'id': 1, 'itemName': 'isAdmin' },
+    { 'id': 2, 'itemName': 'isSubAdmin' },
+    { 'id': 3, 'itemName': 'isBuyer' },
+    { 'id': 4, 'itemName': 'isSupplier' },
+    { 'id': 5, 'itemName': 'isReseller' },
+    { 'id': 6, 'itemName': 'isAgent' },
+    { 'id': 7, 'itemName': 'isAccountAdmin' }]
+  selectedPermission: any = [];
+  showPermissionFlag: boolean = false;
+  settings2 = {
+    singleSelection: false,
+    text: "Select Category",
+    enableSearchFilter: true
+  };
   @Input() currentUser;
   constructor(
     public activeModal: NgbActiveModal,
     private userService: UserService
-    ) { }
+  ) { }
 
   ngOnInit() {
     this.createForm();
   }
 
-  createForm(){
+  createForm() {
     console.log("current user", this.currentUser);
     let firstName = this.currentUser.firstName ? this.currentUser.firstName : '';
     let lastName = this.currentUser.lastName ? this.currentUser.lastName : '';
     let email = this.currentUser.email ? this.currentUser.email : '';
     let password = this.currentUser.password ? this.currentUser.password : '';
-    let status = this.currentUser.status ? this.currentUser.status: '';
+    let status = this.currentUser.status ? this.currentUser.status : '';
     let mobile = this.currentUser.mobile ? this.currentUser.mobile : null;
     let role = this.currentUser.role ? this.currentUser.role : null;
+    let permissionss = this.currentUser.permissions ? this.currentUser.permissions : null;
+
+
+    if (permissionss) {
+      let temp = []
+      console.log("--------------------->>>", permissionss)
+      if (permissionss.isAdmin == true) temp.push('isAdmin');
+      if (permissionss.isSubAdmin == true) temp.push('isSubAdmin');
+      if (permissionss.isBuyer == true) temp.push('isBuyer');
+      if (permissionss.isSupplier == true) temp.push('isSupplier');
+      if (permissionss.isReseller == true) temp.push('isReseller');
+      if (permissionss.isAgent == true) temp.push('isAgent');
+      if (permissionss.isAccountAdmin == true) temp.push('isAccountAdmin');
+      temp.forEach((element, index) => {
+        let item = { 'id': index + 1, 'itemName': element };
+        this.selectedPermission.push(item);
+      });
+    }
+
     this.updateUserForm = new FormGroup({
       'firstName': new FormControl(firstName),
       'lastName': new FormControl(lastName),
@@ -41,24 +78,39 @@ export class EditUserComponent implements OnInit {
       'password': new FormControl(password),
       'status': new FormControl(status),
       'mobile': new FormControl(mobile),
-      'role': new FormControl(role)
+      'role': new FormControl(role),
+      'permissions': new FormControl(permissionss)
     })
+    this.onRoleSelect();
   }
 
-  updateUser(){
+  updateUser() {
+    let permission = { isAdmin: false, isSubAdmin: false, isBuyer: false, isSupplier: false, isReseller: false, isAgent: false, isAccountAdmin: false }
+    if (this.selectedPermission.length > 0 && this.updateUserForm.value.role == 'SubAdmin') {
+      this.selectedPermission.forEach(element => {
+        if (element.itemName == 'isAdmin') permission.isAdmin = true;
+        if (element.itemName == 'isSubAdmin') permission.isSubAdmin = true;
+        if (element.itemName == 'isBuyer') permission.isBuyer = true;
+        if (element.itemName == 'isSupplier') permission.isSupplier = true;
+        if (element.itemName == 'isReseller') permission.isReseller = true;
+        if (element.itemName == 'isAgent') permission.isAgent = true;
+        if (element.itemName == 'isAccountAdmin') permission.isAccountAdmin = true;
+      });
+    }
+    this.updateUserForm.value.permissions = permission;
     this.userService.updateUser(this.currentUser._id, this.updateUserForm.value)
-    .subscribe((response: HttpResponse<any>) => {
-      if(response.status === 200){
+      .subscribe((response: HttpResponse<any>) => {
+        if (response.status === 200) {
+          this.closeModal();
+          this.openSuccessSwal();
+        }
+      }, (error) => {
         this.closeModal();
-        this.openSuccessSwal();
-      }
-    }, (error) => {
-      this.closeModal();
-      console.log("eror", error)
-      this.showMessage = error.error['message']
-      this.openUnscuccessSwal();
-    })
-    
+        console.log("eror", error)
+        this.showMessage = error.error['message']
+        this.openUnscuccessSwal();
+      })
+
   }
 
   openSuccessSwal() {
@@ -81,13 +133,38 @@ export class EditUserComponent implements OnInit {
     this.activeModal.close('Modal Closed');
   }
 
-  cancelNewUserAddition(){
+  cancelNewUserAddition() {
     this.updateUserForm.reset();
     this.closeModal();
   }
 
-  clearModal(){
+  clearModal() {
     this.updateUserForm.reset();
+  }
+
+
+  onRoleSelect() {
+    if (this.updateUserForm.value.role == "SubAdmin") {
+      this.showPermissionFlag = true;
+    }
+    else {
+      this.showPermissionFlag = false;
+    }
+  }
+
+  OnItemDeSelect(item: any) {
+
+  }
+  onSelectAll(items: any) {
+
+  }
+
+  onDeSelectAll(items: any) {
+
+  }
+
+  onItemSelectCat(item: any) {
+
   }
 
 }
