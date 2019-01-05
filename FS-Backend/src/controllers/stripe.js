b067886f1996a9168f6065be831db17b3252d521
Arvind Singh <c-bhattm@kapsarc.org>
2019-01-05 15:31:47 +0530

    test

--- FS-Backend/src/controllers/stripe.js
+++ FS-Backend/src/controllers/stripe.js
@@ -1,4 +1,6 @@
-const stripe = require('stripe')('sk_live_ZJ7T9ooIICoqzBscW0cq20pc');
+// const stripe = require('stripe')('sk_live_ZJ7T9ooIICoqzBscW0cq20pc');
+
+const stripe = require('stripe')('sk_test_1PxLhQXh3FHIMCcB3oAkIIpT');
 
 // Create a payment from a test card token.
 async function createCharge(amount, currency, source, description) {
